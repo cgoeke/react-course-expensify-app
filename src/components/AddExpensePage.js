69050e0839e1f19a5b41ca6bfde9b5d8dea27ba5
Christian Goeke <christianpgoeke@gmail.com>
2018-07-23 11:43:01 -0700

    sets up test database environment

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses';
+import { startAddExpense } from '../actions/expenses';
 
 export const AddExpensePage = (props) => (
     <div>
         <h1>Add Expense Component</h1>
         <ExpenseForm 
             onSubmit={(expense) => {
-                props.addExpense(expense);
+                props.startAddExpense(expense);
                 props.history.push('/');
             }}
         />
@@ -17,7 +17,7 @@ export const AddExpensePage = (props) => (
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        addExpense: (expense) => dispatch(addExpense(expense))
+        startAddExpense: (expense) => dispatch(startAddExpense(expense))
     };
 };
 
